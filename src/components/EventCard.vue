fa7f587280c34a3cc4589937d1e15911a9a18be6
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2020-09-11 21:11:17 +0300

    Added axios call into a modular service, improved EventShow component and EventList component

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-link class="router-link" :to="{ name: 'event-show', params: {id: '1'}}">
+  <router-link class="router-link" :to="{ name: 'event-show', params: {id: event.id}}">
     <v-card class="pa-10">
       <span>@{{event.time}} on {{event.date}}</span>
       <h4>{{event.title}}</h4>
@@ -12,29 +12,8 @@
 
 <script>
 export default {
-  data() {
-    return {
-      event: {
-        id: 1,
-        title: 'Beach Cleanup',
-        date: 'Thu Sep 10, 2020',
-        time: '6:30',
-        attendees: [
-          {
-            id: 'x01',
-            name: 'Karl Opeinheimer'
-          },
-          {
-            id: 'x02',
-            name: 'Adam Longbow'
-          },
-          {
-            id: 'x03',
-            name: 'Martina Kurzsmith'
-          }
-        ]
-      }
-    };
+  props: {
+    event: Object
   }
 };
 </script>
