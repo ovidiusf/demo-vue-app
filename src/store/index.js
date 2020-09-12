716cb302bb47845277331b4db06398e6412f6c64
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2020-09-12 23:45:05 +0300

    Started adding vuex into the proj

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,28 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    user: {
+      id: 'abc123',
+      name: 'Adam Jahr'
+    },
+    categories: ['sustainability', 'education', 'food', 'community']
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
+  mutations: {},
+  actions: {},
+  modules: {},
+  getters: {
+    categoriesLength: state => {
+      return state.categories.length;
+    },
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done);
+    },
+    activeTodosCount: (state, getter) => {
+      return state.todos.length - getters.doneTodos.length;
+    }
   }
-})
+});
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,9 +1,25 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an Event, {{user.name}}</h1>
+    <p>This event was created by {{user.id}}</p>
+    <p>There are {{getCategoriesLength}} categories</p>
+    <ul>
+      <li v-for="category in categories" :key="category">{{category}}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {};
+import { mapState } from 'vuex';
+
+export default {
+  computed: {
+    ...mapState(['user', 'categories']),
+    getCategoriesLength() {
+      return this.$store.getters.categoriesLength;
+    }
+  }
+};
 </script>
 
 <style>
