71f1b3eeb8786f20b46cfd33810a7e84ab589bdf
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2020-09-22 20:45:42 +0300

    Fix link in services

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000',
+  // baseURL: 'http://localhost:3000',
+  baseURL: 'https://json-server-dummy.herokuapp.com',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
