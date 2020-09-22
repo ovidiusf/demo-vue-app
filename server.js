8f0b75afe944dac7119c7fd4084416e34bfac213
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2020-09-22 20:39:34 +0300

    fixed server file, moved logic for json-server to separate folder/repo

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
-
 const express = require('express');
 const serveStatic = require('serve-static');
 const path = require('path');
-app = express();
+let app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const portApp = process.env.PORT || 80;
+const portApp = process.env.PORT || 5000;
+console.log('server started for vue ' + portApp);
 app.listen(portApp);
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
